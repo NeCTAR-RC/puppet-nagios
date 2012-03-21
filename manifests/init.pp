6dfc17f360cb367b51826d4d5d1b2ffeaab9127f
Sam Morrison <sorrison@gmail.com>
2012-03-21 13:01:22 +1100

    Put nagios user in puppet group

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,39 +1,40 @@
 class nagios {
-	
-	package { 'nagios-plugins-basic':
-		ensure => present,
-	}
-
-	user { 'nagios':
-		ensure => present,
-		gid => 'users',
-		groups => 'users',
-		shell => '/bin/bash',
-		home => '/home/nagios',
-		managehome => true,
-		before => File['/home/nagios/.ssh'],
-	}
-
-	file { '/home/nagios/.ssh':
-		ensure => directory,
-		require => User['nagios'],
-		mode => 0600,
-		owner => 'nagios',
-		before => File['/home/nagios/.ssh/authorized_keys'],
-	}
-
-	file { '/home/nagios/.ssh/authorized_keys':
-		ensure => file,
-		mode => 600,
-		owner => 'nagios',
-		require => File['/home/nagios/.ssh'],
-		source => 'puppet:///modules/nagios/nagios_authorized_keys',
-	}
-
-	file { '/home/nagios/libexec':
-		ensure => symlink,
-		target => '/usr/lib/nagios/plugins',
-		require => [User['nagios'],Package['nagios-plugins-basic']],
-	}
+  
+  package {'nagios-plugins-basic':
+	ensure => present,
+  }
+  
+  user {'nagios':
+	ensure     => present,
+	gid        => 'users',
+	groups     => ['users', 'puppet'],
+	shell      => '/bin/bash',
+	home       => '/home/nagios',
+	managehome => true,
+  }
+  
+  file {'/home/nagios/.ssh':
+	ensure  => directory,
+	owner   => nagios,
+    group   => nagios,
+	mode    => '0600',
+	require => User['nagios'],
+  }
+  
+  file {'/home/nagios/.ssh/authorized_keys':
+	ensure  => file,
+	owner   => nagios,
+    group   => nagios,
+	mode    => '0600',
+	source  => 'puppet:///modules/nagios/nagios_authorized_keys',
+	require => File['/home/nagios/.ssh'],
+  }
+  
+  file {'/home/nagios/libexec':
+	ensure  => symlink,
+	target  => '/usr/lib/nagios/plugins',
+	require => [ User['nagios'],
+                 Package['nagios-plugins-basic']],
+  }
 
 }

26d0f856366e004e80e4638fd2a177f7c4e25b7b
Sam Morrison <sorrison@gmail.com>
2012-04-27 11:10:22 +1000

    tidy

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,37 +1,37 @@
 class nagios {
   
   package {'nagios-plugins-basic':
-	ensure => present,
+    ensure => present,
   }
   
   user {'nagios':
-	ensure     => present,
-	gid        => 'users',
-	groups     => ['users', 'puppet'],
-	shell      => '/bin/bash',
-	home       => '/home/nagios',
-	managehome => true,
+    ensure     => present,
+    gid        => 'users',
+    groups     => ['users', 'puppet'],
+    shell      => '/bin/bash',
+    home       => '/home/nagios',
+    managehome => true,
   }
   
   file {'/home/nagios/.ssh':
-	ensure  => directory,
-	owner   => nagios,
-	mode    => '0600',
-	require => User['nagios'],
+    ensure  => directory,
+    owner   => nagios,
+    mode    => '0600',
+    require => User['nagios'],
   }
   
   file {'/home/nagios/.ssh/authorized_keys':
-	ensure  => file,
-	owner   => nagios,
-	mode    => '0600',
-	source  => 'puppet:///modules/nagios/nagios_authorized_keys',
-	require => File['/home/nagios/.ssh'],
+    ensure  => file,
+    owner   => nagios,
+    mode    => '0600',
+    source  => 'puppet:///modules/nagios/nagios_authorized_keys',
+    require => File['/home/nagios/.ssh'],
   }
   
   file {'/home/nagios/libexec':
-	ensure  => symlink,
-	target  => '/usr/lib/nagios/plugins',
-	require => [ User['nagios'],
+    ensure  => symlink,
+    target  => '/usr/lib/nagios/plugins',
+    require => [ User['nagios'],
                  Package['nagios-plugins-basic']],
   }
 

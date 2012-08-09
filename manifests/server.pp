c3b572aaa3d6202093f91472aa9e937a45bd7f32
Russell Sim <russell.sim@gmail.com>
2012-08-09 10:21:06 +1000

    Removed unused checks.

--- manifests/server.pp
+++ manifests/server.pp
@@ -145,60 +145,10 @@ class nagios::server {
     group   => nagios;
   }
 
-
   nagios_command {
-    'check_dummy':
-      command_line => '$USER1$/check_dummy $ARG1$';
-    'check_https_cert':
-      command_line => '$USER1$/check_http --ssl -C 20 -H $HOSTADDRESS$ -I $HOSTADDRESS$';
-    'check_http_url':
-      command_line => '$USER1$/check_http -H $ARG1$ -u $ARG2$';
-    'check_http_url_regex':
-      command_line => '$USER1$/check_http -H $ARG1$ -u $ARG2$ -e $ARG3$';
-    'check_https_url':
-      command_line => '$USER1$/check_http --ssl -H $ARG1$ -u $ARG2$';
-    'check_https_url_regex':
-      command_line => '$USER1$/check_http --ssl -H $ARG1$ -u $ARG2$ -e $ARG3$';
-    'check_mysql_db':
-      command_line => '$USER1$/check_mysql -H $ARG1$ -P $ARG2$ -u $ARG3$ -p $ARG4$ -d $ARG5$';
-    'check_ntp_time':
-      command_line => '$USER1$/check_ntp_time -H $HOSTADDRESS$ -w 0.5 -c 1';
-    'check_silc':
-      command_line => '$USER1$/check_tcp -p 706 -H $ARG1$';
-    'check_sobby':
-      command_line => '$USER1$/check_tcp -H $ARG1$ -p $ARG2$';
-
-    # from apache module
     'http_port':
       command_line => '$USER1$/check_http -p $ARG1$ -H $HOSTADDRESS$ -I $HOSTADDRESS$';
     'https_port':
       command_line => '$USER1$/check_http --ssl -p $ARG1$ -H $HOSTADDRESS$ -I $HOSTADDRESS$';
-
-    'check_http_port_url_content':
-      command_line => '$USER1$/check_http -H $ARG1$ -p $ARG2$ -u $ARG3$ -s $ARG4$';
-    'check_https_port_url_content':
-      command_line => '$USER1$/check_http --ssl -H $ARG1$ -p $ARG2$ -u $ARG3$ -s $ARG4$';
-    'check_http_url_content':
-      command_line => '$USER1$/check_http -H $ARG1$ -u $ARG2$ -s $ARG3$';
-    'check_https_url_content':
-      command_line => '$USER1$/check_http --ssl -H $ARG1$ -u $ARG2$ -s $ARG3$';
-
-    # from bind module
-    'check_dig2':
-      command_line => '$USER1$/check_dig -H $HOSTADDRESS$ -l $ARG1$ --record_type=$ARG2$';
-
-    # from mysql module
-    'check_mysql_health':
-      command_line => '$USER1$/check_mysql_health --hostname $ARG1$ --port $ARG2$ --username $ARG3$ --password $ARG4$ --mode $ARG5$ --database $ARG6$';
-
-    # better check_dns
-    'check_dns2':
-      command_line => '$USER1$/check_dns2 -c $ARG1$ A $ARG2$';
-
-    # dnsbl checking
-    'check_dnsbl':
-      command_line => '$USER1$/check_dnsbl -H $ARG1$';
-
   }
-
 }
--- manifests/target.pp
+++ manifests/target.pp
@@ -1,3 +1,9 @@
+# XXX (russell) I have decided that using this class is a bad idea
+# because it prevents overiding the host groups in the sub classes.
+# This also means that a nagios host class can be added to the base
+# class which prevents breakage when there is no host defined for a
+# service.
+
 class nagios::target (
   $parents = 'absent',
   $address = $::ipaddress,

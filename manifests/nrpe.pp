d306559107988c2c2957336653ef04f4c1f36264
Sam Morrison <sorrison@gmail.com>
2012-08-22 17:23:10 +1000

    Virtualise nrpe packages to make then installed on only hosts that need them

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -1,31 +1,55 @@
 class nagios::nrpe {
 
-  package {
+  @package {
     'nagios-nrpe-server':
-      ensure => present;
+      ensure => present,
+      tag    => 'nrpe';
     'nagios-nrpe-plugin':
-      ensure => present;
+      ensure => present,
+      tag    => 'nrpe';
     'nagios-plugins':
-      ensure => present;
+      ensure => present,
+      tag    => 'nrpe';
   }
 
-  service { 'nagios-nrpe-server':
-    ensure => running,
+  @service { 'nagios-nrpe-server':
+    ensure   => running,
+    require  => Package['nagios-nrpe-server'],
+    tag      => 'nrpe',
   }
 
-  file { '/etc/nagios/nrpe.cfg':
+  @file { '/etc/nagios/nrpe.cfg':
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
     require => Package['nagios-nrpe-server'],
     notify  => Service['nagios-nrpe-server'],
-    content => template('nagios/nrpe.erb');
+    content => template('nagios/nrpe.erb'),
+    tag     => 'nrpe',
   }
 
+  @file {
+    '/usr/local/lib/nagios/':
+      ensure  => directory,
+      owner   => root,
+      group   => root,
+      mode    => '0755',
+      tag     => 'nrpe';
+    '/usr/local/lib/nagios/plugins':
+      ensure  => directory,
+      owner   => root,
+      group   => root,
+      mode    => '0755',
+      tag     => 'nrpe';
+  }
 }
 
-define nagios::nrpe::command ($check_command)
-{
+define nagios::nrpe::command ($check_command) {
+
+  File <| tag == 'nrpe' |>
+  Package <| tag == 'nrpe' |>
+  Service <| tag == 'nrpe' |>
+
   file { "/etc/nagios/nrpe.d/${name}.cfg":
     mode    => '0644',
     owner   => 'root',

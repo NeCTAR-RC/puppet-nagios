36dee15a0d41d8c83473a71bdfffa8d9ea997ea4
Sam Morrison <sorrison@gmail.com>
2013-03-08 11:56:28 +1100

    Removed some old stuff
    Change-Id: Ibb50598ef161c4a9679ab49fd1a80d0f7c752cee

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,50 +1,25 @@
 class nagios {
 
-  package {'nagios-plugins-basic':
-    ensure => present,
-  }
-
-  package {'libipc-run-perl':
-    ensure => present,
-  }
-
-  user {'nagios':
-    ensure     => present,
-    gid        => nagios,
-    groups     => ['users', 'puppet'],
-    shell      => '/bin/false',
-    home       => '/var/lib/nagios',
-  }
-
-  file {'/home/nagios':
-    ensure => absent,
-    force  => true,
-  }
-
-  file { '/usr/lib/nagios/plugins/check_md_raid':
-    ensure => absent,
-  }
-
-  file { '/usr/lib/nagios/plugins/check_memcached.py':
-    ensure  => absent,
-  }
-
   file { '/etc/sudoers.d/sudoers_nagios':
     owner   => 'root',
     group   => 'root',
     mode    => 440,
     source  => 'puppet:///modules/nagios/sudoers_nagios',
-    require => Package['nagios-plugins-basic'],
+    require => Package['nagios-plugins'],
   }
 
   if $virtual == "physical" {
+    package {'libipc-run-perl':
+      ensure => present,
+    }
+
     file { '/usr/lib/nagios/plugins/check_ipmi_sensor.pl':
       ensure  => file,
       owner   => root,
       group   => root,
       mode    => '0755',
       source  => 'puppet:///modules/nagios/check_ipmi_sensor.pl',
-      require => Package['nagios-plugins-basic','libipc-run-perl'],
+      require => Package['nagios-plugins','libipc-run-perl'],
     }
   }
 

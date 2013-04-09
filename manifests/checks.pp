4eea7d545af827000027856a51a430b93a9dec55
Marcus Furlong <furlongm@gmail.com>
2013-04-09 17:42:09 +1000

    clean up nagios class try 2
    Change-Id: I3c1f3586c5b2bee45ca2a1aef331955ccdfaa31c

---
+++ manifests/checks.pp
@@ -0,0 +1,25 @@
+class nagios::checks::iostat {
+
+  package { 'sysstat':
+    ensure => present;
+  }
+
+  file{ '/usr/local/lib/nagios/plugins/check_iostat':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///modules/nagios/check_iostat',
+  }
+
+  nagios::nrpe::service { 'check_iostat':
+    check_command => '/usr/local/lib/nagios/plugins/check_iostat -d vdb -c 2,75,2 -w 1,60,1';
+  }
+}
+
+class nagios::checks::md_raid {
+
+  nagios::nrpe::service { 'check_md_raid':
+    check_command => '/usr/lib/nagios/plugins/check_md_raid';
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,28 +1,5 @@
 class nagios {
 
-  file { '/etc/sudoers.d/sudoers_nagios':
-    owner   => 'root',
-    group   => 'root',
-    mode    => 440,
-    source  => 'puppet:///modules/nagios/sudoers_nagios',
-    require => Package['nagios-plugins'],
-  }
-
-  if $virtual == "physical" {
-    package {'libipc-run-perl':
-      ensure => present,
-    }
-
-    file { '/usr/lib/nagios/plugins/check_ipmi_sensor.pl':
-      ensure  => file,
-      owner   => root,
-      group   => root,
-      mode    => '0755',
-      source  => 'puppet:///modules/nagios/check_ipmi_sensor.pl',
-      require => Package['nagios-plugins','libipc-run-perl'],
-    }
-  }
-
 }
 
 define nagios::command ($check_command) {
@@ -33,5 +10,4 @@ define nagios::command ($check_command) {
     notify  => Service['nagios3'],
     content => template('nagios/nagios_command.erb');
   }
-
 }

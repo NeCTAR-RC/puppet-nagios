2c75f9aa8d77dff88625c3c7215111880eb54345
Sam Morrison <sorrison@gmail.com>
2013-08-01 10:43:04 +1000

    Remove reliance on other openstack modules
    Change-Id: I062ddd468fa0cb6594d0cc26fc3822d82cb60e5f

--- manifests/openstack-checks.pp
+++
@@ -1,9 +0,0 @@
-class nagios::openstack-checks {
-
-  include nova::api::nagios-checks
-  include swift::proxy::nagios-checks
-  include glance::registry::nagios-checks
-  include keystone::nagios-checks
-  include glance::nagios-checks
-
-}

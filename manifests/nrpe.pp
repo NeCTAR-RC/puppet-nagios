cf9da31944f9d3fe7f37550cb600558cf4e2c3c2
Sam Morrison <sorrison@gmail.com>
2014-03-13 14:44:47 +1100

    Make it easier to have nrpe services without using nagios
    Change-Id: I1efb990615ceb44aac9689d41860622247bb6a31

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -101,24 +101,28 @@ define nagios::nrpe::service (
   $service_description = 'absent',
   $nrpe_command = 'check_nrpe_1arg',
   ) {
-  nagios::nrpe::command {
-    $name:
-      check_command => $check_command;
-  }
 
-  nagios::service {
-    $name:
-      check_command         => "${nrpe_command}!${name}",
-      check_period          => $check_period,
-      normal_check_interval => $normal_check_interval,
-      retry_check_interval  => $retry_check_interval,
-      max_check_attempts    => $max_check_attempts,
-      notification_interval => $notification_interval,
-      notification_period   => $notification_period,
-      notification_options  => $notification_options,
-      contact_groups        => $contact_groups,
-      use                   => $use,
-      service_description   => $service_description,
-      servicegroups         => $servicegroups,
+  # Only add NRPE checks if this host is using NRPE
+  if defined(Service['nagios-nrpe-server']) {
+    nagios::nrpe::command {
+      $name:
+        check_command => $check_command;
+    }
+
+    nagios::service {
+      $name:
+        check_command         => "${nrpe_command}!${name}",
+        check_period          => $check_period,
+        normal_check_interval => $normal_check_interval,
+        retry_check_interval  => $retry_check_interval,
+        max_check_attempts    => $max_check_attempts,
+        notification_interval => $notification_interval,
+        notification_period   => $notification_period,
+        notification_options  => $notification_options,
+        contact_groups        => $contact_groups,
+        use                   => $use,
+        service_description   => $service_description,
+        servicegroups         => $servicegroups,
+    }
   }
 }

21d83bda0b463e5b047396a65543d8d2e5a57ae1
Andrew Spiers <andrew@andrewspiers.net>
2014-11-26 14:54:24 +1100

    Nrpe: Support Red Hat 5 based systems.
    Change-Id: Ib723ffa165dc85d83cc79f9f0b80ecaaae167a04

---
+++ manifests/nrpe/service.pp
@@ -0,0 +1,42 @@
+#Type to create the nagios config to poll nrpe checks.
+define nagios::nrpe::service (
+  $check_command,
+  $check_period = '',
+  $normal_check_interval = '',
+  $retry_check_interval = '',
+  $max_check_attempts = '',
+  $notification_interval = '',
+  $notification_period = '',
+  $notification_options = '',
+  $contact_groups = '',
+  $servicegroups = '',
+  $use = hiera('nagios::service::use', 'generic-service'),
+  $service_description = 'absent',
+  $nrpe_command = 'check_nrpe_1arg',
+  ) {
+
+  # Only add NRPE checks if this host is using NRPE
+  #if defined(Service['nagios-nrpe-server']) {
+  if defined(Service[ 'nagios-nrpe-server' ]) {
+    nagios::nrpe::command {
+      $name:
+        check_command => $check_command;
+    }
+
+    nagios::service {
+      $name:
+        check_command         => "${nrpe_command}!${name}",
+        check_period          => $check_period,
+        normal_check_interval => $normal_check_interval,
+        retry_check_interval  => $retry_check_interval,
+        max_check_attempts    => $max_check_attempts,
+        notification_interval => $notification_interval,
+        notification_period   => $notification_period,
+        notification_options  => $notification_options,
+        contact_groups        => $contact_groups,
+        use                   => $use,
+        service_description   => $service_description,
+        servicegroups         => $servicegroups,
+    }
+  }
+}

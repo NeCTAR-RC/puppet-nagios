eaded071effbaaadfa57e26c52fdd20727f2aaf7
Andy Botting <andy@andybotting.com>
2015-12-03 11:23:52 +1100

    Add pushbullet notification support
    Change-Id: I5fd081f25c121547800c0b81c7ee15cbc001e4f9
    NOTE: Requires hiera changes first

--- manifests/contact.pp
+++ manifests/contact.pp
@@ -1,29 +1,45 @@
 define nagios::contact (
-  $email                         = undef,
-  $host_notification_commands    = 'notify-host-by-email',
+  $email,
+  $pushbullet_api_key            = undef,
+  $host_notification_commands    = undef,
   $host_notification_options     = 'd,r',
   $host_notification_period,
-  $service_notification_commands = 'notify-service-by-email',
+  $service_notification_commands = undef,
   $service_notification_options  = 'w,u,c,r',
   $service_notification_period,
 ) {
 
-  if ($email == undef) {
-    $contact_email = $name
+  if $host_notification_commands {
+    $real_host_notification_commands = $host_notification_commands
   } else {
-    $contact_email = $email
+    if $pushbullet_api_key {
+      $real_host_notification_commands = 'notify-host-by-email,notify-host-by-pushbullet'
+    } else {
+      $real_host_notification_commands = 'notify-host-by-email'
+    }
   }
 
- nagios_contact {
-    $name:
-      tag                           => $::environment,
-      alias                         => $name,
-      email                         => $contact_email,
-      host_notification_commands    => $host_notification_commands,
-      host_notification_options     => $host_notification_options,
-      host_notification_period      => $host_notification_period,
-      service_notification_commands => $service_notification_commands,
-      service_notification_options  => $service_notification_options,
-      service_notification_period   => $service_notification_period,
- }
+  if $service_notification_commands {
+    $real_service_notification_commands = $service_notification_commands
+  } else {
+    if $pushbullet_api_key {
+      $real_service_notification_commands = 'notify-service-by-email,notify-service-by-pushbullet'
+    } else {
+      $real_service_notification_commands = 'notify-service-by-email'
+    }
+  }
+
+   nagios_contact {
+     $name:
+       tag                           => $::environment,
+       alias                         => $name,
+       email                         => $email,
+       pager                         => $pushbullet_api_key,
+       host_notification_commands    => $real_host_notification_commands,
+       host_notification_options     => $host_notification_options,
+       host_notification_period      => $host_notification_period,
+       service_notification_commands => $real_service_notification_commands,
+       service_notification_options  => $service_notification_options,
+       service_notification_period   => $service_notification_period,
+   }
 }

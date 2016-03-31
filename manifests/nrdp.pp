9233bbd8be66e05d3ba35dfaba9b177af9a32060
Andy Botting <andy@andybotting.com>
2016-03-31 11:51:24 +1100

    Add NRDP to Nagios server
    Change-Id: Ifd3b418b80a52f1a98cd86bd9bb1c14c0972c9bc

---
+++ manifests/nrdp.pp
@@ -0,0 +1,88 @@
+# Set up the Nagios NRDP PHP application
+class nagios::nrdp {
+
+  $nrdp_tokens = hiera('nagios::nrdp_tokens', [])
+
+  file { '/var/lib/nagios3/tmp':
+    ensure => directory,
+    mode   => 0770,
+    owner  => 'nagios',
+    group  => 'www-data',
+  }
+
+  file { [ '/opt/nrdp',
+           '/opt/nrdp/includes',
+           '/opt/nrdp/plugins',
+           '/opt/nrdp/plugins/nagioscorecmd',
+           '/opt/nrdp/plugins/nagioscorepassivecheck']:
+    ensure => directory,
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { '/opt/nrdp/config.inc.php':
+    content => template('nagios/nrdp-config.inc.php.erb'),
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => File['/opt/nrdp'],
+  }
+
+
+  file { '/opt/nrdp/index.php':
+    source  => 'puppet:///modules/nagios/nrdp/server/index.php',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => File['/opt/nrdp'],
+  }
+
+  file { '/opt/nrdp/includes/utils.inc.php':
+    source  => 'puppet:///modules/nagios/nrdp/server/includes/utils.inc.php',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => File['/opt/nrdp/includes'],
+  }
+
+  file { '/opt/nrdp/includes/constants.inc.php':
+    source  => 'puppet:///modules/nagios/nrdp/server/includes/constants.inc.php',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => File['/opt/nrdp/includes'],
+  }
+
+  file { '/opt/nrdp/plugins/nagioscorecmd/nagioscorecmd.inc.php':
+    source  => 'puppet:///modules/nagios/nrdp/server/plugins/nagioscorecmd/nagioscorecmd.inc.php',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => File['/opt/nrdp/plugins/nagioscorecmd'],
+  }
+
+  file { '/opt/nrdp/plugins/nagioscorepassivecheck/nagioscorepassivecheck.inc.php':
+    source  => 'puppet:///modules/nagios/nrdp/server/plugins/nagioscorepassivecheck/nagioscorepassivecheck.inc.php',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => File['/opt/nrdp/plugins/nagioscorepassivecheck'],
+  }
+
+  file { '/etc/apache2/conf-available/nrdp.conf':
+    source  => 'puppet:///modules/nagios/nrdp/apache-nrdp.conf',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+  }
+
+  exec { '/usr/sbin/a2enconf nrdp':
+    creates => "/etc/apache2/conf-enabled/nrdp.conf",
+    notify  => Service['apache2'],
+    require => [
+      File['/etc/apache2/conf-available/nrdp.conf'],
+    ]
+  }
+
+}

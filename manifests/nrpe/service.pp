8bd00ad0294fc22ff835e2fe90ef6d45519d86f7
Andy Botting <andy@andybotting.com>
2016-07-22 13:24:52 +1000

    Use undef as default for nrpe service
    Change-Id: I659957f052a65901f1b9c80f7ee6a446cc4bfa2d

--- manifests/nrpe/service.pp
+++ manifests/nrpe/service.pp
@@ -1,20 +1,19 @@
 #Type to create the nagios config to poll nrpe checks.
 define nagios::nrpe::service (
   $check_command,
-  $check_period = '',
-  $normal_check_interval = '',
-  $retry_check_interval = '',
-  $max_check_attempts = '',
-  $notification_interval = '',
-  $notification_period = '',
-  $notification_options = '',
-  $contact_groups = '',
-  $servicegroups = '',
-  $use = hiera('nagios::service::use', 'generic-service'),
-  $service_description = 'absent',
-  $nrpe_command = 'check_nrpe_1arg',
-  )
-{
+  $check_period          = undef,
+  $normal_check_interval = undef,
+  $retry_check_interval  = undef,
+  $max_check_attempts    = undef,
+  $notification_interval = undef,
+  $notification_period   = undef,
+  $notification_options  = undef,
+  $contact_groups        = undef,
+  $servicegroups         = undef,
+  $use                   = hiera('nagios::service::use', 'generic-service'),
+  $service_description   = 'absent',
+  $nrpe_command          = 'check_nrpe_1arg',
+) {
 
   # Only add NRPE checks if this host is using NRPE
   if defined(Service[$nagios::nrpe::nrpe]) {
@@ -23,20 +22,19 @@ define nagios::nrpe::service (
         check_command => $check_command;
     }
 
-    nagios::service {
-      $name:
-        check_command         => "${nrpe_command}!${name}",
-        check_period          => $check_period,
-        normal_check_interval => $normal_check_interval,
-        retry_check_interval  => $retry_check_interval,
-        max_check_attempts    => $max_check_attempts,
-        notification_interval => $notification_interval,
-        notification_period   => $notification_period,
-        notification_options  => $notification_options,
-        contact_groups        => $contact_groups,
-        use                   => $use,
-        service_description   => $service_description,
-        servicegroups         => $servicegroups,
+    nagios::service { $name:
+      check_command         => "${nrpe_command}!${name}",
+      check_period          => $check_period,
+      normal_check_interval => $normal_check_interval,
+      retry_check_interval  => $retry_check_interval,
+      max_check_attempts    => $max_check_attempts,
+      notification_interval => $notification_interval,
+      notification_period   => $notification_period,
+      notification_options  => $notification_options,
+      contact_groups        => $contact_groups,
+      use                   => $use,
+      service_description   => $service_description,
+      servicegroups         => $servicegroups,
     }
   }
 }

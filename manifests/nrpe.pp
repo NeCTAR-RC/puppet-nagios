5d03e7351d191ad5e679f04097e01f77d6a985e7
Jake Yip <jake.yip@unimelb.edu.au>
2017-04-05 17:56:24 +1000

    Puppet3to4: $::osfamily is a string
    Change-Id: Ieff57fc5ce206faf96d59ec73e48f30307e14f83

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -4,27 +4,27 @@ class nagios::nrpe {
   $nagios_hosts = hiera('nagios::hosts', [])
 
   $nrpe = $::osfamily ? {
-    RedHat  =>  'nrpe',
-    Debian  =>  'nagios-nrpe-server',
-    default =>  'nagios-nrpe-server',
+    'RedHat'  =>  'nrpe',
+    'Debian'  =>  'nagios-nrpe-server',
+    default   =>  'nagios-nrpe-server',
   }
 
   $nrpe_user = $::osfamily ? {
-    RedHat  =>  'nrpe',
-    Debian  =>  'nagios',
-    default =>  'nagios',
+    'RedHat'  =>  'nrpe',
+    'Debian'  =>  'nagios',
+    default   =>  'nagios',
   }
 
   $nrpe_plugin = $::osfamily ? {
-    RedHat  =>  'nagios-plugins-nrpe',
-    Debian  =>  'nagios-nrpe-plugin',
-    default =>  'nagios-nrpe-plugin',
+    'RedHat'  =>  'nagios-plugins-nrpe',
+    'Debian'  =>  'nagios-nrpe-plugin',
+    default   =>  'nagios-nrpe-plugin',
   }
 
   $nagios_plugins_contrib = $::osfamily ? {
-    RedHat  =>  'nagios-plugins-all',
-    Debian  =>  'nagios-plugins-contrib',
-    default =>  'nagios-plugins-contrib',
+    'RedHat'  =>  'nagios-plugins-all',
+    'Debian'  =>  'nagios-plugins-contrib',
+    default   =>  'nagios-plugins-contrib',
   }
 
   @package {

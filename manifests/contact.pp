e119906a1f6434cf87c2d25c345a5e5fc91a2e4b
Jake Yip <jake.yip@unimelb.edu.au>
2018-03-01 16:43:52 +1100

    Use $config_environment instead of $environment
    Supports dynamic/dev environments which will have different names than
    the one in production
    
    Change-Id: I2c4716bbd630ce83da1fceb957a1f68a142b7fc7

--- manifests/contact.pp
+++ manifests/contact.pp
@@ -7,6 +7,7 @@ define nagios::contact (
   $service_notification_options  = 'w,u,c,r',
   $service_notification_period,
 ) {
+  $config_environment = hiera('puppet::config_environment', $::environment)
 
   if ($email == undef) {
     $contact_email = $name
@@ -16,7 +17,7 @@ define nagios::contact (
 
  nagios_contact {
     $name:
-      tag                           => $::environment,
+      tag                           => $config_environment,
       alias                         => $name,
       email                         => $contact_email,
       host_notification_commands    => $host_notification_commands,

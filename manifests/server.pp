5378612d2066042894c11e2c7f7bd78c20dbd527
Sam Morrison <sorrison@gmail.com>
2020-08-21 12:20:42 +1000

    Support nagios on focal and above
    Change-Id: I8c03b993e3d1f85e711bf2f7e9ccbfe7b964977c

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,19 +1,19 @@
-class nagios::server {
+class nagios::server inherits nagios::params {
 
   $config_environment = hiera('puppet::config_environment', $::environment)
 
-  $nagios_pkgs = [ 'nagios3', 'nagios-images']
+  $nagios_pkgs = [ $nagios::params::nagios_version, 'nagios-images']
 
   package { $nagios_pkgs:
     ensure => present,
     notify => Exec['nagios_exec_fix', 'nagios_exec_fix1'],
   }
 
-  service { 'nagios3':
+  service { $nagios::params::nagios_version:
     ensure => running,
   }
 
-  file { '/etc/nagios3/nagios.cfg':
+  file { "/etc/${nagios::params::nagios_version}/nagios.cfg":
     ensure  => file,
     owner   => root,
     group   => root,
@@ -23,23 +23,23 @@ class nagios::server {
     notify  => Service['nagios3'],
   }
 
-  file { '/etc/nagios3/extra.d':
+  file { '/etc/${nagios3}/extra.d':
     ensure => directory,
   }
 
   exec {
     'nagios_exec_fix':
-      command => 'dpkg-statoverride --update --add nagios www-data 2710 /var/lib/nagios3/rw',
-      unless  => 'dpkg-statoverride --list /var/lib/nagios3/rw',
+      command => "dpkg-statoverride --update --add nagios www-data 2710 /var/lib/${nagios::params::nagios_version}/rw",
+      unless  => "dpkg-statoverride --list /var/lib/${nagios::params::nagios_version}/rw",
       path    => ['/usr/bin/', '/usr/sbin/'],
-      require => Package['nagios3'],
-      notify  => Service['nagios3'];
+      require => Package[$nagios::params::nagios_version],
+      notify  => Service[$nagios::params::nagios_version];
     'nagios_exec_fix1':
-      command => 'dpkg-statoverride --update --add nagios nagios 751 /var/lib/nagios3',
-      unless  => 'dpkg-statoverride --list /var/lib/nagios3',
+      command => "dpkg-statoverride --update --add nagios nagios 751 /var/lib/${nagios::params::nagios_version}",
+      unless  => "dpkg-statoverride --list /var/lib/${nagios::params::nagios_version}",
       path    => ['/usr/bin/', '/usr/sbin/'],
-      require => Package['nagios3'],
-      notify  => Service['nagios3'];
+      require => Package[$nagios::params::nagios_version],
+      notify  => Service[$nagios::params::nagios_version];
   }
 
   nagios_servicegroup {
@@ -89,105 +89,105 @@ class nagios::server {
 
   Nagios_command <| tag == $config_environment |> {
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
   Nagios_contact <| tag == $config_environment |> {
-    target  => '/etc/nagios3/conf.d/nagios_contact.cfg',
+    target  => "/etc/${nagios::params::nagios_version}/conf.d/nagios_contact.cfg",
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
   Nagios_contactgroup <| tag == $config_environment |> {
-    target  => '/etc/nagios3/conf.d/nagios_contactgroup.cfg',
+    target  => "/etc/${nagios::params::nagios_version}/conf.d/nagios_contactgroup.cfg",
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
   Nagios_host <| tag == $config_environment |> {
-    target  => '/etc/nagios3/conf.d/nagios_host.cfg',
+    target  => "/etc/${nagios::params::nagios_version}/conf.d/nagios_host.cfg",
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
   Nagios_hostdependency <| tag == $config_environment |> {
-    target => '/etc/nagios3/conf.d/nagios_hostdependency.cfg',
-    notify => Service['nagios3'],
+    target => "/etc/${nagios::params::nagios_version}/conf.d/nagios_hostdependency.cfg",
+    notify => Service[$nagios::params::nagios_version],
   }
   Nagios_hostescalation <| tag == $config_environment |> {
-    target => '/etc/nagios3/conf.d/nagios_hostescalation.cfg',
-    notify => Service['nagios3'],
+    target => "/etc/${nagios::params::nagios_version}/conf.d/nagios_hostescalation.cfg",
+    notify => Service[$nagios::params::nagios_version],
   }
   Nagios_hostextinfo <| tag == $config_environment |> {
-    target  => '/etc/nagios3/conf.d/nagios_hostextinfo.cfg',
+    target  => "/etc/${nagios::params::nagios_version}/conf.d/nagios_hostextinfo.cfg",
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
   Nagios_hostgroup <| tag == $config_environment |> {
-    target  => '/etc/nagios3/conf.d/nagios_hostgroup.cfg',
+    target  => "/etc/${nagios::params::nagios_version}/conf.d/nagios_hostgroup.cfg",
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
   Nagios_service <| tag == $config_environment |> {
-    target  => '/etc/nagios3/conf.d/nagios_service.cfg',
+    target  => "/etc/${nagios::params::nagios_version}/conf.d/nagios_service.cfg",
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
   Nagios_servicegroup <| tag == $config_environment |> {
-    target  => '/etc/nagios3/conf.d/nagios_servicegroup.cfg',
+    target  => "/etc/${nagios::params::nagios_version}/conf.d/nagios_servicegroup.cfg",
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
   Nagios_servicedependency <| tag == $config_environment |> {
-    target  => '/etc/nagios3/conf.d/nagios_servicedependency.cfg',
+    target  => "/etc/${nagios::params::nagios_version}/conf.d/nagios_servicedependency.cfg",
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
   Nagios_serviceescalation <| tag == $config_environment |> {
-    target  => '/etc/nagios3/conf.d/nagios_serviceescalation.cfg',
+    target  => "/etc/${nagios::params::nagios_version}/conf.d/nagios_serviceescalation.cfg",
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
   Nagios_serviceextinfo <| tag == $config_environment |> {
-    target  => '/etc/nagios3/conf.d/nagios_serviceextinfo.cfg',
+    target  => "/etc/${nagios::params::nagios_version}/conf.d/nagios_serviceextinfo.cfg",
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
   Nagios_timeperiod <| tag == $config_environment |> {
-    target  => '/etc/nagios3/conf.d/nagios_timeperiod.cfg',
+    target  => "/etc/${nagios::params::nagios_version}/conf.d/nagios_timeperiod.cfg",
     require => File['nagios_confd'],
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
   }
 
-  file { ['/etc/nagios3/conf.d/nagios_contact.cfg',
-          '/etc/nagios3/conf.d/nagios_contactgroup.cfg',
-          '/etc/nagios3/conf.d/nagios_host.cfg',
-          '/etc/nagios3/conf.d/nagios_hostdependency.cfg',
-          '/etc/nagios3/conf.d/nagios_hostescalation.cfg',
-          '/etc/nagios3/conf.d/nagios_hostextinfo.cfg',
-          '/etc/nagios3/conf.d/nagios_hostgroup.cfg',
-          '/etc/nagios3/conf.d/nagios_hostgroupescalation.cfg',
-          '/etc/nagios3/conf.d/nagios_service.cfg',
-          '/etc/nagios3/conf.d/nagios_servicedependency.cfg',
-          '/etc/nagios3/conf.d/nagios_serviceescalation.cfg',
-          '/etc/nagios3/conf.d/nagios_serviceextinfo.cfg',
-          '/etc/nagios3/conf.d/nagios_servicegroup.cfg',
-          '/etc/nagios3/conf.d/nagios_timeperiod.cfg']:
+  file { ["/etc/${nagios::params::nagios_version}/conf.d/nagios_contact.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_contactgroup.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_host.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_hostdependency.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_hostescalation.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_hostextinfo.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_hostgroup.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_hostgroupescalation.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_service.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_servicedependency.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_serviceescalation.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_serviceextinfo.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_servicegroup.cfg",
+          "/etc/${nagios::params::nagios_version}/conf.d/nagios_timeperiod.cfg"]:
             ensure  => file,
             replace => false,
-            notify  => Service['nagios3'],
+            notify  => Service[$nagios::params::nagios_version],
             mode    => '0644',
             owner   => root,
             group   => 0;
   }
 
-  file { '/etc/nagios3/conf.d/nagios_command.cfg':
+  file { "/etc/${nagios::params::nagios_version}/conf.d/nagios_command.cfg":
     ensure  => absent,
   }
 
   file { 'nagios_confd':
     ensure  => directory,
-    path    => '/etc/nagios3/conf.d/',
+    path    => "/etc/${nagios::params::nagios_version}/conf.d/",
     purge   => true,
     recurse => true,
     force   => true,
-    notify  => Service['nagios3'],
+    notify  => Service[$nagios::params::nagios_version],
     mode    => '0644',
     owner   => root,
     group   => nagios;
